212d285f0e5fd66fdf8d0defc5db63b2a1427238
chikundant <46445767+chikundant@users.noreply.github.com>
2019-05-12 11:19:09 +0300

    Add files via upload

---
+++ LastLodeRunnerWithoutMouse/Project1/Settings.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+
+// map
+const int WIDTH = 157;
+const int HEIGHT = 43;
+const int INDENT_TOP = 4;
+const int INDENT_SIDE = 30;
+const int MENU_INDENT = INDENT_SIDE/2 - 5;
+
+const int COUNT_OF_LADERS = 200;
+const int COUNT_OF_BLOCKS = 300;
+const int COUNT_OF_ROPES = 300;
+const int COUNT_OF_PRIZE = 10;
+const int COUNT_OF_ENEMY = 3;
+
+const int BLOCK_WIDTH = 3;
+
+const int ROPE_WIDTH = 1;
+//const int ROPE_HEIGHT = 3;
+
+const int LADER_WIDTH = 5;
+const int LADER_HEIGHT = 6;
+
+const int ITEMS_COUNT = 4;
+
+//graphics
+const char FULL_SIZE_BLOCK = (char)219;
+const char LADER = (char)176;
+const char ROPE = '_';
+const char PRIZE = (char)36;
+
+//keyboard
+const char ENTER = 13;
+const char ESCAPE = 27;
+const char LEFT = 75;
+const char RIGHT = 77;
+const char DOWN = 80;
+const char UP = 72;
+
+//game settings
+const int ENEMY_SPEED = 6000;
---
+++ LastLodeRunnerWithoutMouse/Project1/WindowSettings.cpp
@@ -0,0 +1,13 @@
+#include "WindowSettings.h"
+
+using namespace std;
+
+void ShowConsole()
+{
+	system("title LODE RUNNER");
+	system("color F0"); //F0
+	int screen_width = 1600;//GetSystemMetrics(SM_CXSCREEN); // получение разрешения экрана
+	int screen_height = 1000;//GetSystemMetrics(SM_CYSCREEN);
+	MoveWindow(GetConsoleWindow(), 0, 0, screen_width, screen_height, true);
+	ShowCursor(false);
+}
